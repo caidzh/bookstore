b4abd4d40d09774734b953c8b08e8e1707549e87
caidzh <148761690+caidzh@users.noreply.github.com>
2024-02-13 17:48:21 +0800

    Add files via upload

--- book.h
+++ book.h
@@ -22,25 +22,27 @@ class book{
     public:
         std::array<char,65>isbn,bookname,author,keyword;
         double price;
-        int quantity;
+        int quantity,special;
         bool is_constructed;
         book(){
             price=0;
             quantity=0;
+            special=0;
             isbn=N;
             bookname=N;
             author=N;
             keyword=N;
             is_constructed=false;
         }
-        book(double price,int quantity,bool is_constructed):
-            price(price),quantity(quantity),is_constructed(is_constructed){}
+        book(double price,int quantity,int special,bool is_constructed):
+            price(price),quantity(quantity),special(special),is_constructed(is_constructed){}
         book(const book &x){
             isbn=x.isbn;
             bookname=x.bookname;
             author=x.author;
             keyword=x.keyword;
             quantity=x.quantity;
+            special=x.special;
             price=x.price;
             is_constructed=x.is_constructed;
         }
@@ -53,6 +55,7 @@ class book{
             keyword=x.keyword;
             price=x.price;
             quantity=x.quantity;
+            special=x.special;
             is_constructed=x.is_constructed;
             return *this;
         }
